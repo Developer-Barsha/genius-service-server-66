0e81dba5bfccc09579e3ac8dc7ca9b93aaeca178
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-27 17:00:48 +0600

    added steps

---
+++ __server_deploy.js
@@ -0,0 +1,16 @@
+/* 
+----------------------------
+ONE TIME FRO YOUR COMPUTER
+----------------------------
+*  1. Create heroku account 
+*  2. verify email
+*  3. install heroku cli
+*  4. heroku login
+----------------------------
+FOR EACH PROJECT ONE TIME
+----------------------------
+*  1. heroku create
+*  2. 
+*  3. 
+*  4. 
+*/
