225effa2b703f61a3408f5686596df04606cbae8
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-27 17:14:46 +0600

    changes made

--- __server_deploy.js
+++ __server_deploy.js
@@ -10,7 +10,25 @@ ONE TIME FRO YOUR COMPUTER
 FOR EACH PROJECT ONE TIME
 ----------------------------
 *  1. heroku create
-*  2. 
-*  3. 
-*  4. 
+*  2. make sure you run: hit add., git commit -m"msg", git push
+*  3. git push heroku main
+*  4. Go to heroku Dashboard > Current Project > Settings > Reveal Config Vars
+*  5. Copy paste config vars from your .env file
+*  5. MAke sure you have whitelisted all ip addess to access mongodb 
+*  4. heroku login
+----------------------------
+UPDATE SERVER WITH NEW CHANGES
+----------------------------
+*  1. make changes
+*  2. make sure you run: hit add., git commit -m"msg", git push
+*  3. git push heroku main 
+----------------------------
+FOR EACH PROJECT ONE TIME
+----------------------------
+*  1. heroku create
+*  2. make sure you run: hit add., git commit -m"msg", git push
+*  3. git push heroku main
+*  4. Go to heroku Dashboard > Current Project > Settings > Reveal Config Vars
+*  5. Copy paste config vars from your .env file
+*  5. MAke sure you have whitelisted all ip addess to access mongodb 
 */
--- index.js
+++ index.js
@@ -108,6 +108,10 @@ app.get('/', (req, res) => {
     res.send('running genius server')
 })
 
+app.get('/hero', (req, res) => {
+    res.send('hero meets heroku')
+})
+
 app.listen(port, () => {
     console.log('listening to port: ', port);
 })
